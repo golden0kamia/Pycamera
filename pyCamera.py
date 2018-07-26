a1741f9ae465b75ac633bec6b74f33b18612e2e8
Zérock <golden0kamia@gmail.com>
2018-07-26 18:03:35 +0200

    update

--- pyCamera.py
+++ pyCamera.py
@@ -1,18 +1,23 @@
 from tkinter import *
-from picamera import PiCamera
-import RPi.GPIO as GPIO
 from datetime import datetime
 from time import sleep
 
-camera = PiCamera()
-camera.resolution = (1640, 1232)
+try:
+    from picamera import PiCamera
+    import RPi.GPIO as GPIO
+    camera = PiCamera(resolution = (1640, 1232), framerate = 30)
+    camera.exposure_mode = 'night'
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#while GPIO.input(5):
-#    pass
+    print('check button are not pressed and connected')
+    while GPIO.input(5) and GPIO.input(3):
+        pass
+    print('Start program')
+except:
+    print('error while setting up the raspberry')
 
 sharpnessString = ""
 contrastString = ""
@@ -64,18 +69,20 @@ def ISOCMD(value = 0):
     global isoString
     if isoActivate.get():
         isoScale.config(state=DISABLED)
-        camera.iso = 200
+        camera.iso = 0
     else:
         isoScale.config(state=NORMAL)
         camera.iso = isoScale.get()
 
 def Destroy():
 	print("Quit")
+	camera.close()
 	window.destroy
 	GPIO.cleanup()
 	quit()
 
 window = Tk()
+#window.attributes('-fullscreen', True)
 
 sharpnessActivate = BooleanVar()
 contrastActivate = BooleanVar()
@@ -88,89 +95,89 @@ view = Frame(window, bg=backGround)
 view.pack(fill=BOTH, expand=1)
 
 sharpnessFrame = Frame(view, bg=backGround, relief=SUNKEN, bd=2)
-sharpnessFrame.pack(fill=X, expand=1)
+sharpnessFrame.grid(row=1, column=1, columnspan=2)
 
 sharpnessLabel = Label(sharpnessFrame, text="Sharpness", bg=backGround, fg=foreGround, padx=2)
-sharpnessLabel.pack(side=LEFT)
-
-sharpnessCheck = Checkbutton(sharpnessFrame, bg=backGround, fg=foreGround, text="Auto", command=SharpnessCMD, variable=sharpnessActivate)
-sharpnessCheck.pack(side=RIGHT)
-sharpnessCheck.select()
+sharpnessLabel.grid(row=1, column=1)
 
 sharpnessScale = Scale(sharpnessFrame, length=300, from_=-100, to=100, bg=backGround, fg=foreGround, orient=HORIZONTAL, command=SharpnessCMD)
-sharpnessScale.pack(side=RIGHT, fill=X, expand=1)
+sharpnessScale.grid(row=1, column=2)
 sharpnessScale.set(0)
 sharpnessScale.config(state=DISABLED)
 
+sharpnessCheck = Checkbutton(sharpnessFrame, bg=backGround, fg=foreGround, text="Auto", command=SharpnessCMD, variable=sharpnessActivate)
+sharpnessCheck.grid(row=1, column=3)
+sharpnessCheck.select()
+
 
 contrastFrame = Frame(view, bg=backGround, relief=SUNKEN, bd=2)
-contrastFrame.pack(fill=X, expand=1)
+contrastFrame.grid(row=2, column=1, columnspan=2)
 
 contrastLabel = Label(contrastFrame, text="Contrast", bg=backGround, fg=foreGround, padx=6)
-contrastLabel.pack(side=LEFT)
-
-contrastCheck = Checkbutton(contrastFrame, bg=backGround, fg=foreGround, text="Auto", command=ContrastCMD, variable=contrastActivate)
-contrastCheck.pack(side=RIGHT)
-contrastCheck.select()
+contrastLabel.grid(row=1, column=1)
 
 contrastScale = Scale(contrastFrame, length=300, from_=-100, to=100, bg=backGround, fg=foreGround, orient=HORIZONTAL, command=ContrastCMD)
-contrastScale.pack(side=RIGHT, fill=X, expand=1)
+contrastScale.grid(row=1, column=2)
 contrastScale.set(0)
 contrastScale.config(state=DISABLED)
 
+contrastCheck = Checkbutton(contrastFrame, bg=backGround, fg=foreGround, text="Auto", command=ContrastCMD, variable=contrastActivate)
+contrastCheck.grid(row=1, column=3)
+contrastCheck.select()
+
 
 brightnessFrame = Frame(view, bg=backGround, relief=SUNKEN, bd=2)
-brightnessFrame.pack(fill=X, expand=1)
+brightnessFrame.grid(row=3, column=1, columnspan=2)
 
 brightnessLabel = Label(brightnessFrame, text="Brightness", bg=backGround, fg=foreGround)
-brightnessLabel.pack(side=LEFT)
-
-brightnessCheck = Checkbutton(brightnessFrame, bg=backGround, fg=foreGround, text="Auto", command=BrightnessCMD, variable=brightnessActivate)
-brightnessCheck.pack(side=RIGHT)
-brightnessCheck.select()
+brightnessLabel.grid(row=1, column=1)
 
 brightnessScale = Scale(brightnessFrame, length=300, from_=0, to=100, bg=backGround, fg=foreGround, orient=HORIZONTAL, command=BrightnessCMD)
-brightnessScale.pack(side=RIGHT, fill=X, expand=1)
+brightnessScale.grid(row=1, column=2)
 brightnessScale.set(50)
 brightnessScale.config(state=DISABLED)
 
+brightnessCheck = Checkbutton(brightnessFrame, bg=backGround, fg=foreGround, text="Auto", command=BrightnessCMD, variable=brightnessActivate)
+brightnessCheck.grid(row=1, column=3)
+brightnessCheck.select()
+
 
 saturationFrame = Frame(view, bg=backGround, relief=SUNKEN, bd=2)
-saturationFrame.pack(fill=X, expand=1)
+saturationFrame.grid(row=4, column=1, columnspan=2)
 
 saturationLabel = Label(saturationFrame, text="Saturation", bg=backGround, fg=foreGround, padx=1)
-saturationLabel.pack(side=LEFT)
-
-saturationCheck = Checkbutton(saturationFrame, bg=backGround, fg=foreGround, text="Auto", command=SaturationCMD, variable=saturationActivate)
-saturationCheck.pack(side=RIGHT)
-saturationCheck.select()
+saturationLabel.grid(row=1, column=1)
 
 saturationScale = Scale(saturationFrame, length=300, from_=-100, to=100, bg=backGround, fg=foreGround, orient=HORIZONTAL, command=SaturationCMD)
-saturationScale.pack(side=RIGHT, fill=X, expand=1)
+saturationScale.grid(row=1, column=2)
 saturationScale.set(0)
 saturationScale.config(state=DISABLED)
 
+saturationCheck = Checkbutton(saturationFrame, bg=backGround, fg=foreGround, text="Auto", command=SaturationCMD, variable=saturationActivate)
+saturationCheck.grid(row=1, column=3)
+saturationCheck.select()
+
 
 isoFrame = Frame(view, bg=backGround, relief=SUNKEN, bd=2)
-isoFrame.pack(fill=X, expand=1)
+isoFrame.grid(row=5, column=1, columnspan=2)
 
 isoLabel = Label(isoFrame, text="ISO", bg=backGround, fg=foreGround, padx=19)
-isoLabel.pack(side=LEFT)
+isoLabel.grid(row=1, column=1)
 
-isoCheck = Checkbutton(isoFrame, bg=backGround, fg=foreGround, text="Auto", command=ISOCMD, variable=isoActivate)
-isoCheck.pack(side=RIGHT)
-isoCheck.select()
-
-isoScale = Scale(isoFrame, length=300, from_=100, to=800, bg=backGround, fg=foreGround, orient=HORIZONTAL, command=ISOCMD)
-isoScale.pack(side=RIGHT, fill=X, expand=1)
+isoScale = Scale(isoFrame, length=300, from_=100, to=800, resolution=100, bg=backGround, fg=foreGround, orient=HORIZONTAL, command=ISOCMD)
+isoScale.grid(row=1, column=2)
 isoScale.config(state=DISABLED)
 
+isoCheck = Checkbutton(isoFrame, bg=backGround, fg=foreGround, text="Auto", command=ISOCMD, variable=isoActivate)
+isoCheck.grid(row=1, column=3)
+isoCheck.select()
 
-openCamera = Button(view, text="Camera", bg=backGround, fg=foreGround, command=camera.start_preview(), bd=7)
-openCamera.pack(fill=X, expand=1, side=LEFT)
+
+openCamera = Button(view, text="Camera", bg=backGround, fg=foreGround, command=lambda:camera.start_preview(), bd=7)
+openCamera.grid(row=6, column=1)
 
 quitCamera = Button(view, text="Quit", bg=backGround, fg=foreGround, command=Destroy, bd=7)
-quitCamera.pack(fill=X, expand=1, side=RIGHT)
+quitCamera.grid(row=6, column=2)
 
 window.mainloop()
 
@@ -187,11 +194,11 @@ while True:
                 if sd >= 15:
                     break
             if sd >= 15:
-                camera.start_recording("%s.jpg" %datetime.now().strftime('%Y-%m-%d_%H:%M:%S'))
+                camera.start_recording("%s.mjpeg" %datetime.now().strftime('%Y-%m-%d_%H:%M:%S'), 'mjpeg')
                 vFlag = True
             else:
-                camera.capture("%s.jpg" %datetime.now().strftime('%Y-%m-%d_%H:%M:%S'))
+                camera.capture("%s.png" %datetime.now().strftime('%Y.%m.%d_%H:%M:%S'), 'png')
 
-    if not GPIO.input(3):
+    if GPIO.input(3):
         camera.quit_preview
 
